d3f701c7dfdd87ebeb89a66dcf41773bdef716a9
Roobiah <roobiahmurtadho@gmail.com>
2022-03-10 15:56:27 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main -Where programs are executed
+ *
+ * Return: 0
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
-	return(0);
-
+	puts("Programing is like building a multilingual puzzle\n");
+	return (0);
 }
