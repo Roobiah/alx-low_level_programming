b9a5bffbf9499072a079c2ff360ef90d15d91bdc
Roobiah <roobiahmurtadho@gmail.com>
2022-03-14 15:28:32 -0700

    positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,6 @@
  *  main - Entry point
  *
  *  Return: Always 0 (Success)
- *
  */
 int main(void)
 {
@@ -13,6 +12,7 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
--- README.md
+++
@@ -1,1 +0,0 @@
-first task on C
